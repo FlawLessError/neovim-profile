a63672b6970a80ffdfe5a9476ac70754213605b9
FlawLessError <flawlesserrordev@gmail.com>
2024-11-22 21:22:59 +0100

    removed toggleterm and fixed the fashboard collision

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,16 +8,6 @@ local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
 vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
 
--- toggleTerm keybindings
-vim.api.nvim_set_keymap("n", "<c-t>", '<Cmd>exe v:count1 . "ToggleTerm"<CR>', opts)
-
-local cmp = require("cmp")
-cmp.setup({
-  mapping = {
-    ["<C-b>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-  },
-})
-
 -- activate Minty
 vim.keymap.set("n", "<c-z>", function()
   require("minty.huefy").open()
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,9 +21,11 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.vscode" },
 
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
 
     -- import/override with your plugins
