6dc28666913b72a208ee52b172d4fcc0bbb1a698
FlawLessError <flawlesserrordev@gmail.com>
2024-12-10 14:59:58 +0100

    added markdown review and render 'even though review doesn't work maybe i'll work in linux.'

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,204 @@
+return {
+
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      enabled = true,
+      sign = true,
+      style = "full",
+      position = "left",
+      language_pad = 0,
+      language_name = true,
+      disable_background = { "diff" },
+      width = "full",
+      left_margin = 0,
+      left_pad = 0,
+      right_pad = 0,
+      min_width = 0,
+      border = "thin",
+      above = "▄",
+      below = "▀",
+      highlight = "RenderMarkdownCode",
+      highlight_inline = "RenderMarkdownCodeInline",
+      highlight_language = nil,
+    },
+    heading = {
+      enabled = true,
+      sign = true,
+      position = "overlay",
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      signs = { "󰫎 " },
+      width = "full",
+      left_margin = 0,
+      left_pad = 0,
+      right_pad = 0,
+      min_width = 0,
+      border = false,
+      border_virtual = false,
+      border_prefix = false,
+      above = "▄",
+      below = "▀",
+      backgrounds = {
+        "RenderMarkdownH1Bg",
+        "RenderMarkdownH2Bg",
+        "RenderMarkdownH3Bg",
+        "RenderMarkdownH4Bg",
+        "RenderMarkdownH5Bg",
+        "RenderMarkdownH6Bg",
+      },
+      foregrounds = {
+        "RenderMarkdownH1",
+        "RenderMarkdownH2",
+        "RenderMarkdownH3",
+        "RenderMarkdownH4",
+        "RenderMarkdownH5",
+        "RenderMarkdownH6",
+      },
+    },
+    dash = {
+      enabled = true,
+      icon = "─",
+      width = "full",
+      highlight = "RenderMarkdownDash",
+    },
+    bullet = {
+      enabled = true,
+      icons = { "●", "○", "◆", "◇" },
+      ordered_icons = function(level, index, value)
+        value = vim.trim(value)
+        local value_index = tonumber(value:sub(1, #value - 1))
+        return string.format("%d.", value_index > 1 and value_index or index)
+      end,
+      left_pad = 0,
+      right_pad = 0,
+      highlight = "RenderMarkdownBullet",
+    },
+    checkbox = {
+      enabled = true,
+      position = "inline",
+      unchecked = {
+        icon = "󰄱 ",
+        highlight = "RenderMarkdownUnchecked",
+        scope_highlight = nil,
+      },
+      checked = {
+        icon = "󰱒 ",
+        highlight = "RenderMarkdownChecked",
+        scope_highlight = nil,
+      },
+      custom = {
+        todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+      },
+    },
+    quote = {
+      enabled = true,
+      icon = "▋",
+      repeat_linebreak = false,
+      highlight = "RenderMarkdownQuote",
+    },
+    pipe_table = {
+      enabled = true,
+      preset = "none",
+      style = "full",
+      cell = "padded",
+      padding = 1,
+      min_width = 0,
+      border = {
+        "┌",
+        "┬",
+        "┐",
+        "├",
+        "┼",
+        "┤",
+        "└",
+        "┴",
+        "┘",
+        "│",
+        "─",
+      },
+      alignment_indicator = "━",
+      head = "RenderMarkdownTableHead",
+      row = "RenderMarkdownTableRow",
+      filler = "RenderMarkdownTableFill",
+    },
+    callout = {
+      note = { raw = "[!NOTE]", rendered = "󰋽 Note", highlight = "RenderMarkdownInfo" },
+      tip = { raw = "[!TIP]", rendered = "󰌶 Tip", highlight = "RenderMarkdownSuccess" },
+      important = { raw = "[!IMPORTANT]", rendered = "󰅾 Important", highlight = "RenderMarkdownHint" },
+      warning = { raw = "[!WARNING]", rendered = "󰀪 Warning", highlight = "RenderMarkdownWarn" },
+      caution = { raw = "[!CAUTION]", rendered = "󰳦 Caution", highlight = "RenderMarkdownError" },
+      abstract = { raw = "[!ABSTRACT]", rendered = "󰨸 Abstract", highlight = "RenderMarkdownInfo" },
+      summary = { raw = "[!SUMMARY]", rendered = "󰨸 Summary", highlight = "RenderMarkdownInfo" },
+      tldr = { raw = "[!TLDR]", rendered = "󰨸 Tldr", highlight = "RenderMarkdownInfo" },
+      info = { raw = "[!INFO]", rendered = "󰋽 Info", highlight = "RenderMarkdownInfo" },
+      todo = { raw = "[!TODO]", rendered = "󰗡 Todo", highlight = "RenderMarkdownInfo" },
+      hint = { raw = "[!HINT]", rendered = "󰌶 Hint", highlight = "RenderMarkdownSuccess" },
+      success = { raw = "[!SUCCESS]", rendered = "󰄬 Success", highlight = "RenderMarkdownSuccess" },
+      check = { raw = "[!CHECK]", rendered = "󰄬 Check", highlight = "RenderMarkdownSuccess" },
+      done = { raw = "[!DONE]", rendered = "󰄬 Done", highlight = "RenderMarkdownSuccess" },
+      question = { raw = "[!QUESTION]", rendered = "󰘥 Question", highlight = "RenderMarkdownWarn" },
+      help = { raw = "[!HELP]", rendered = "󰘥 Help", highlight = "RenderMarkdownWarn" },
+      faq = { raw = "[!FAQ]", rendered = "󰘥 Faq", highlight = "RenderMarkdownWarn" },
+      attention = { raw = "[!ATTENTION]", rendered = "󰀪 Attention", highlight = "RenderMarkdownWarn" },
+      failure = { raw = "[!FAILURE]", rendered = "󰅖 Failure", highlight = "RenderMarkdownError" },
+      fail = { raw = "[!FAIL]", rendered = "󰅖 Fail", highlight = "RenderMarkdownError" },
+      missing = { raw = "[!MISSING]", rendered = "󰅖 Missing", highlight = "RenderMarkdownError" },
+      danger = { raw = "[!DANGER]", rendered = "󱐌 Danger", highlight = "RenderMarkdownError" },
+      error = { raw = "[!ERROR]", rendered = "󱐌 Error", highlight = "RenderMarkdownError" },
+      bug = { raw = "[!BUG]", rendered = "󰨰 Bug", highlight = "RenderMarkdownError" },
+      example = { raw = "[!EXAMPLE]", rendered = "󰉹 Example", highlight = "RenderMarkdownHint" },
+      quote = { raw = "[!QUOTE]", rendered = "󱆨 Quote", highlight = "RenderMarkdownQuote" },
+      cite = { raw = "[!CITE]", rendered = "󱆨 Cite", highlight = "RenderMarkdownQuote" },
+    },
+    link = {
+      enabled = true,
+      footnote = {
+        superscript = true,
+        prefix = "",
+        suffix = "",
+      },
+      image = "󰥶 ",
+      email = "󰀓 ",
+      hyperlink = "󰌹 ",
+      highlight = "RenderMarkdownLink",
+      wiki = { icon = "󱗖 ", highlight = "RenderMarkdownWikiLink" },
+      custom = {
+        web = { pattern = "^http", icon = "󰖟 " },
+        youtube = { pattern = "youtube%.com", icon = "󰗃 " },
+        github = { pattern = "github%.com", icon = "󰊤 " },
+        neovim = { pattern = "neovim%.io", icon = " " },
+        stackoverflow = { pattern = "stackoverflow%.com", icon = "󰓌 " },
+        discord = { pattern = "discord%.com", icon = "󰙯 " },
+        reddit = { pattern = "reddit%.com", icon = "󰑍 " },
+      },
+    },
+    sign = {
+      enabled = true,
+      highlight = "RenderMarkdownSign",
+    },
+    indent = {
+      enabled = false,
+      per_level = 2,
+      skip_level = 1,
+      skip_heading = false,
+    },
+  },
+  ft = { "markdown", "norg", "rmd", "org" },
+  config = function(_, opts)
+    require("render-markdown").setup(opts)
+    Snacks.toggle({
+      name = "Render Markdown",
+      get = function()
+        return require("render-markdown.state").enabled
+      end,
+      set = function(enabled)
+        local m = require("render-markdown")
+        if enabled then
+          m.enable()
+        else
+          m.disable()
+        end
+      end,
+    }):map("<leader>um")
+  end,
+}
