c2852fd2ddc62d74d6ef8c6970220479205d1ec3
FlawLessError <flawlesserrordev@gmail.com>
2024-12-15 18:01:24 +0100

    Some major updates to lazyvim
    Telescope was replaced with fzf
    and I added a new config file for choco packages that I use in windows

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.vscode" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
 
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
