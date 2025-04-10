2c06545c5c63c9932224d8b7fc0c40b40a793936
FlawLessError <flawlesserrordev@gmail.com>
2025-04-10 14:52:27 +0100

    Slightly modify bufferline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- enables true colors
+vim.opt.termguicolors = true
+
 vim.cmd("set scrolloff=8")
 -- vim.cmd("set tabstop=2")
 -- vim.cmd("set softtabstop=2")
