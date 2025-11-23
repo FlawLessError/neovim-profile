db84b3687ca6de22422cf201451da2b0c4b89a8e
FlawLessError <50498066+FlawLessError@users.noreply.github.com>
2025-11-23 15:58:14 +0100

    Fix neovim
    Missing component (maybe new update?) reason (away from computer for
    more than 2 months)

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,29 +1,29 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   version = false, -- last release is way too old and doesn't work on Windows
-  build = ":TSUpdate",
-  event = { "LazyFile", "VeryLazy" },
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  init = function(plugin)
-    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-    -- Luckily, the only things that those plugins need are the custom queries, which we make available
-    -- during startup.
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
+  build = function()
+    local TS = require("nvim-treesitter")
+    if not TS.get_installed then
+      LazyVim.error("Please restart Neovim and run `:TSUpdate` to use the `nvim-treesitter` **main** branch.")
+      return
+    end
+    -- make sure we're using the latest treesitter util
+    package.loaded["lazyvim.util.treesitter"] = nil
+    LazyVim.treesitter.build(function()
+      TS.update(nil, { summary = true })
+    end)
   end,
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  keys = {
-    { "<c-space>", desc = "Increment Selection" },
-    { "<bs>", desc = "Decrement Selection", mode = "x" },
-  },
+  event = { "LazyFile", "VeryLazy" },
+  cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
   opts_extend = { "ensure_installed" },
-  ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
+  ---@alias lazyvim.TSFeat { enable?: boolean, disable?: string[] }
+  ---@class lazyvim.TSConfig: TSConfig
   opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
+    -- LazyVim config for treesitter
+    indent = { enable = true }, ---@type lazyvim.TSFeat
+    highlight = { enable = true }, ---@type lazyvim.TSFeat
+    folds = { enable = true }, ---@type lazyvim.TSFeat
     ensure_installed = {
       "bash",
       "c",
@@ -50,33 +50,83 @@ return {
       "xml",
       "yaml",
     },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-    textobjects = {
-      move = {
-        enable = true,
-        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      },
-    },
   },
-  ---@param opts TSConfig
+  ---@param opts lazyvim.TSConfig
   config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    local TS = require("nvim-treesitter")
+
+    setmetatable(require("nvim-treesitter.install"), {
+      __newindex = function(_, k)
+        if k == "compilers" then
+          vim.schedule(function()
+            LazyVim.error({
+              "Setting custom compilers for `nvim-treesitter` is no longer supported.",
+              "",
+              "For more info, see:",
+              "- [compilers](https://docs.rs/cc/latest/cc/#compile-time-requirements)",
+            })
+          end)
+        end
+      end,
+    })
+
+    -- some quick sanity checks
+    if not TS.get_installed then
+      return LazyVim.error("Please use `:Lazy` and update `nvim-treesitter`")
+    elseif type(opts.ensure_installed) ~= "table" then
+      return LazyVim.error("`nvim-treesitter` opts.ensure_installed must be a table")
     end
 
-    vim.treesitter.language.register("html", "ejs")
+    -- setup treesitter
+    TS.setup(opts)
+    LazyVim.treesitter.get_installed(true) -- initialize the installed langs
 
-    require("nvim-treesitter.configs").setup(opts)
+    -- install missing parsers
+    local install = vim.tbl_filter(function(lang)
+      return not LazyVim.treesitter.have(lang)
+    end, opts.ensure_installed or {})
+    if #install > 0 then
+      LazyVim.treesitter.build(function()
+        TS.install(install, { summary = true }):await(function()
+          LazyVim.treesitter.get_installed(true) -- refresh the installed langs
+        end)
+      end)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("lazyvim_treesitter", { clear = true }),
+      callback = function(ev)
+        local ft, lang = ev.match, vim.treesitter.language.get_lang(ev.match)
+        if not LazyVim.treesitter.have(ft) then
+          return
+        end
+
+        ---@param feat string
+        ---@param query string
+        local function enabled(feat, query)
+          local f = opts[feat] or {} ---@type lazyvim.TSFeat
+          return f.enable ~= false
+            and not (type(f.disable) == "table" and vim.tbl_contains(f.disable, lang))
+            and LazyVim.treesitter.have(ft, query)
+        end
+
+        -- highlighting
+        if enabled("highlight", "highlights") then
+          pcall(vim.treesitter.start, ev.buf)
+        end
+
+        -- indents
+        if enabled("indent", "indents") then
+          LazyVim.set_default("indentexpr", "v:lua.LazyVim.treesitter.indentexpr()")
+        end
+
+        -- folds
+        if enabled("folds", "folds") then
+          if LazyVim.set_default("foldmethod", "expr") then
+            LazyVim.set_default("foldexpr", "v:lua.LazyVim.treesitter.foldexpr()")
+          end
+        end
+      end,
+    })
   end,
 }
