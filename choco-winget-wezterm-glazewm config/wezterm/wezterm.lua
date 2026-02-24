48688425da859a1359efe06836ab8e287cf41110
FlawLessError <50498066+FlawLessError@users.noreply.github.com>
2026-02-24 02:20:24 +0000

    Update wezterm config.
    Change style of tabBar

--- choco-winget-wezterm-glazewm config/wezterm/wezterm.lua
+++ choco-winget-wezterm-glazewm config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local commands = require("commands")
 local config = wezterm.config_builder()
 
 -- Font settings
-config.font_size = 13
+config.font_size = 15
 config.line_height = 1
 config.font = wezterm.font("DepartureMono Nerd Font")
 
@@ -27,7 +27,7 @@ config.window_padding = {
 }
 
 -- Miscellaneous settings
-config.max_fps = 120
+config.max_fps = 60 -- was 120
 config.prefer_egl = true
 config.default_prog = { "pwsh.exe" }
 
@@ -35,5 +35,45 @@ config.default_prog = { "pwsh.exe" }
 wezterm.on("augment-command-palette", function()
   return commands
 end)
+
+local tabFontColorActive = "#d80982"
+local tabFontColorInActive = "#b00669"
+local titleBarColor = "#0b0022"
+
+config.window_frame = {
+  font = wezterm.font({ family = "DepartureMono Nerd Font" }),
+  font_size = 12.0,
+  active_titlebar_bg = titleBarColor,
+}
+
+config.colors = {
+  tab_bar = {
+    active_tab = {
+      bg_color = "#150046",
+      fg_color = tabFontColorActive,
+    },
+
+    inactive_tab_edge = "None",
+
+    inactive_tab = {
+      bg_color = "None",
+      fg_color = tabFontColorInActive,
+    },
+
+    inactive_tab_hover = {
+      bg_color = "None",
+      fg_color = tabFontColorInActive,
+    },
+
+    new_tab = {
+      bg_color = "None",
+      fg_color = titleBarColor,
+    },
+    new_tab_hover = {
+      bg_color = "None",
+      fg_color = "None",
+    },
+  },
+}
 
 return config
