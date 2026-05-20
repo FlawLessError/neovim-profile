9f6d3238d75a857c0366fd62c4fd16ea52e801b6
FlawLessError <50498066+FlawLessError@users.noreply.github.com>
2026-05-20 19:08:13 +0100

    Changed the default terminal for nvim to powershell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- use pwsh instead of cmd
+LazyVim.terminal.setup("pwsh")
+
 -- enables true colors
 vim.opt.termguicolors = true
 
