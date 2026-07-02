714dba15d8d774a1d6fc771f1191b2680eb524f6
FlawLessError <50498066+FlawLessError@users.noreply.github.com>
2026-07-02 18:30:19 +0100

    Update Lazy lua, added options some config to options and added a file stylua
    All of that was to deal with the annoyance of tab gets reset to 2
    instead of the specified one that is 4, part of that was the
    "EditorConfig
    "

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,17 +9,21 @@ LazyVim.terminal.setup("pwsh")
 vim.opt.termguicolors = true
 
 -- number of extra lines/columns to keep between cursor and edge of screen
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 
 -- disable additional junk files
 -- https://gitlab.com/jokeyrhyme/dotfiles/-/blob/main/config/nvim/init.lua#L5-11
-vim.o.swapfile = false
+vim.opt.swapfile = false
 
 -- tab stuff
-vim.o.smarttab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 8
+vim.opt.smarttab = true
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+
+-- bypass editorConfig
+vim.g.editorconfig = false
 
 -- for folding a block of code
 -- vim.cmd("set foldmethod=indent")
@@ -27,51 +31,51 @@ vim.o.shiftwidth = 8
 -- auto-reload files when modified externally
 -- https://unix.stackexchange.com/a/383044
 -- https://gitlab.com/jokeyrhyme/dotfiles/-/blob/main/config/nvim/init.lua#L5-11
-vim.o.autoread = true
+vim.opt.autoread = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = { "*" },
 })
 
 local function enforceColors()
-  -- Set colors for LineNr
-  vim.api.nvim_set_hl(0, "LineNr", { fg = "#C40879", bg = "White", bold = true })
-  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#2B8FF3", bold = true })
-  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#B50603", bold = true })
+    -- Set colors for LineNr
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#C40879", bg = "White", bold = true })
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#2B8FF3", bold = true })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#B50603", bold = true })
 
-  -- Set CursorLine and CursorLineNr colors
-  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ad5fff", bg = "#4f009e", bold = true })
-  -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#f4eafe" })
-  -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#ad5fff" })
+    -- Set CursorLine and CursorLineNr colors
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ad5fff", bg = "#4f009e", bold = true })
+    -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#f4eafe" })
+    -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#ad5fff" })
 
-  -- Set TreesitterContext colors
-  -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#100500" })
-  -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "#ff9a0e" })
-  -- vim.api.nvim_set_hl(0, "TreesitterContextBottom", { bg = "#100500" })
-  -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { bg = "#100500" })
+    -- Set TreesitterContext colors
+    -- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#100500" })
+    -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "#ff9a0e" })
+    -- vim.api.nvim_set_hl(0, "TreesitterContextBottom", { bg = "#100500" })
+    -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { bg = "#100500" })
 
-  -- set which-key colors
-  -- vim.api.nvim_set_hl(0, "WhichKey", { bg = "#100500" })
-  -- vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "#ff9a0e" })
-  -- vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "#100500" })
-  -- vim.api.nvim_set_hl(0, "WhichKeySeparator", { fg = "#ff9a0e" })
-  -- vim.api.nvim_set_hl(0, "WhichKeyDesc", { fg = "#ffffff" })
-  -- vim.api.nvim_set_hl(0, "WhichKeyGroup", { fg = "#ff9a0e" })
-  -- vim.api.nvim_set_hl(0, "WhichKey", { fg = "#ffffff" })
-  -- vim.api.nvim_set_hl(0, "WhichKeyIconBlue", { fg = "#ff9a0e" })
+    -- set which-key colors
+    -- vim.api.nvim_set_hl(0, "WhichKey", { bg = "#100500" })
+    -- vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "#ff9a0e" })
+    -- vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "#100500" })
+    -- vim.api.nvim_set_hl(0, "WhichKeySeparator", { fg = "#ff9a0e" })
+    -- vim.api.nvim_set_hl(0, "WhichKeyDesc", { fg = "#ffffff" })
+    -- vim.api.nvim_set_hl(0, "WhichKeyGroup", { fg = "#ff9a0e" })
+    -- vim.api.nvim_set_hl(0, "WhichKey", { fg = "#ffffff" })
+    -- vim.api.nvim_set_hl(0, "WhichKeyIconBlue", { fg = "#ff9a0e" })
 
-  -- Set GitSigns colors
-  vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#95CB2A" })
-  vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#5B4BE7" })
-  vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#C30505" })
+    -- Set GitSigns colors
+    vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#95CB2A" })
+    vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#5B4BE7" })
+    vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#C30505" })
 
-  -- Set NeoTree Git Signs
-  vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#95CB2A" })
-  vim.api.nvim_set_hl(0, "NeoTreeGitConflict", { fg = "#c7c700" })
-  vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#C30505" })
-  vim.api.nvim_set_hl(0, "NeoTreeGitIgnored", { fg = "#c5c0f6" })
-  vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#5B4BE7" })
-  vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#4ae78e" })
+    -- Set NeoTree Git Signs
+    vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#95CB2A" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitConflict", { fg = "#c7c700" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#C30505" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitIgnored", { fg = "#c5c0f6" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#5B4BE7" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#4ae78e" })
 end
 
 -- Call the function to apply the settings
@@ -79,5 +83,5 @@ enforceColors()
 
 -- Optionally, use an autocommand to reapply these colors when the colorscheme is changed
 vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = enforceColors,
+    callback = enforceColors,
 })
